fac0cbeb978b5c380324b8938e3b9482cf33cbb4
marc-enzo.bonnafon <marcenzo.bonnafon@gmail.com>
2023-07-04 22:47:14 +0200

    setup ci for terraform

--- tp2_terraform/main.tf
+++ tp2_terraform/main.tf
@@ -1,10 +1,13 @@
 resource "github_repository" "tp_cesi" {
-  name        = "tp-cesi"
+  name = "tp-cesi"
 
-  visibility = "public"
+  visibility    = "public"
   has_downloads = true
-  has_issues = true
-  has_projects = true
-  has_wiki = true
+  has_issues    = true
+  has_projects  = true
+  has_wiki      = true
 
+  allow_merge_commit = false
+  allow_rebase_merge = false
+  allow_squash_merge = false
 }
